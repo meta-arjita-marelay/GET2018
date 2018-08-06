d8725655f244b0fd71989cbeea153b31341d6f8f
meta-arjita-marelay <41181680+meta-arjita-marelay@users.noreply.github.com>
2018-08-06 15:00:56 +0530

    DS-Assignment-2

---
+++ DS-Assignment-2/Question4/StudentNode.java
@@ -0,0 +1,37 @@
+package Question4;
+
+/**
+ * Generic node class to implement Linked List
+ * 
+ * @author Arjita
+ *
+ */
+public class StudentNode {
+
+	private String name;
+	private String[] data;
+	private StudentNode next;
+
+	public StudentNode(String name, String[] data) {
+		this.name = name;
+		this.data = data;
+		this.next = null;
+	}
+
+	public StudentNode getNext() {
+		return next;
+	}
+
+	public void setNext(StudentNode next) {
+		this.next = next;
+	}
+
+	public String[] getData() {
+		return this.data;
+	}
+
+	public String getName() {
+		return this.name;
+	}
+
+}
