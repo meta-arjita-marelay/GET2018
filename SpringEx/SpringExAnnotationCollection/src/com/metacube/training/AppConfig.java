7ab9bb0788aa872c18b3083a781210cbac34a6b7
Arjita Marelay <arjita.marelay@metacube.com>
2018-09-06 15:30:27 +0530

    SpringEx

---
+++ SpringEx/SpringExAnnotationCollection/src/com/metacube/training/AppConfig.java
@@ -0,0 +1,12 @@
+package com.metacube.training;
+
+import java.util.List;
+
+public class AppConfig {
+	public JavaCollection javaCollection(){
+		JavaCollection javaCollection = new JavaCollection();
+		List addresslistList;
+		
+		return javaCollection;
+	}
+}
