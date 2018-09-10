b31dd3f8ca6bf63a02c264d9707f0c0b1c67b482
meta-arjita-marelay <41181680+meta-arjita-marelay@users.noreply.github.com>
2018-09-11 01:27:56 +0530

    EAD-Assignment-9

---
+++ EmployeePortal9/src/main/java/com/metacube/training/mappers/ProjectMapper.java
@@ -0,0 +1,29 @@
+package com.metacube.training.mappers;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import org.springframework.jdbc.core.RowMapper;
+import com.metacube.training.model.ProjectMaster;
+
+/**
+ * ProjectMapper will map the project object with database query result
+ * 
+ * @author Arjita
+ *
+ */
+public class ProjectMapper implements RowMapper<ProjectMaster> {
+
+	public ProjectMaster mapRow(ResultSet resultSet, int i) throws SQLException {
+
+		ProjectMaster projectMaster = new ProjectMaster();
+		projectMaster.setProjectId(resultSet.getInt("project_id"));
+		projectMaster.setName(resultSet.getString("name"));
+		projectMaster.setDescription(resultSet.getString("description"));
+		projectMaster.setStartDate(resultSet.getDate("start_date"));
+		projectMaster.setEndDate(resultSet.getDate("end_date"));
+		projectMaster.setProjectLogo(resultSet.getString("project_logo"));
+
+		return projectMaster;
+	}
+}
