9e15b5ff8ef2b54578b4e2aeb576c4b0665a0b20
Arjita Marelay <arjita.marelay@metacube.com>
2018-09-25 13:22:49 +0530

    EAD-Assignment-13 Spring security

---
+++ EmployeePortalSpringSecurity/src/main/java/com/metacube/training/repository/SkillsRepository.java
@@ -0,0 +1,10 @@
+package com.metacube.training.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.metacube.training.model.Project;
+import com.metacube.training.model.SkillsMaster;
+
+public interface SkillsRepository <P> extends JpaRepository<SkillsMaster, Long> {
+	SkillsMaster findBySkillId(int id);
+}
