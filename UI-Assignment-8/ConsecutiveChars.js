eeac7a91b7a385010ac933a4f92e90c194f118e8
Arjita Marelay <arjita.marelay@metacube.com>
2018-10-16 13:18:13 +0530

    UI-Assignment-8

--- UI-Assignment-8/ConsecutiveChars.js
+++ UI-Assignment-8/ConsecutiveChars.js
@@ -1,20 +1,24 @@
-function removeConsecutiveCharacters(inputString) {
-    var i = 0,
-        flag = 0;
-       
-    for (i = 0; i <= inputString.length - 2; i++) {
-        var tempString = inputString[i];
-        while (inputString[i] == inputString[i + 1] ) {
-            flag = 1;
-            tempString = tempString + inputString[i + 1];
-            i++;
+function removeConsecutiveRepeatingString(inputString) {
+    var i = 0;
+    
+    while(i < inputString.length - 1) {
+        var n = 0;
+        var j = i;
+        while(inputString.charAt(j) == inputString.charAt(j + 1)) {
+            n++;
+            j++;
         }
-        if (flag == 1) {
-            inputString = inputString.replace(tempString, "");
-            flag = 0;
-            i = -1;
+            
+        if(n > 0) {
+            inputString = inputString.substring(0, i) + inputString.substring(i + n + 1, inputString.length);
+            console.log(inputString);
+            i = i - n - 1;
         }
+
+        i++;
+        if(i < 0)
+            i = 0;
     }
+    
     return inputString;
 }
- 
