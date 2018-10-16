fe76295fbe12af5c2ffe7d44ad6075269f1c60c6
Arjita Marelay <arjita.marelay@metacube.com>
2018-10-16 13:25:14 +0530

    UI-Assignment-8

--- UI-Assignment-8/Queue.js
+++ UI-Assignment-8/Queue.js
@@ -1,28 +1,34 @@
 function Queue() {
-    this._oldestIndex = 1;
-    this._newestIndex = 1;
+    this.rear = 0;
+    this.front = 0;
     this._storage = {};
 }
  
 Queue.prototype.size = function() {
-    return this._newestIndex - this._oldestIndex;
+    return this.front - this.rear;
 };
  
 Queue.prototype.enqueue = function(data) {
-    this._storage[this._newestIndex] = data;
-    this._newestIndex++;
+    this._storage[this.front] = data;
+    this.front++;
 };
  
 Queue.prototype.dequeue = function() {
-    var oldestIndex = this._oldestIndex,
-        newestIndex = this._newestIndex,
+    var oldestIndex = this.rear,
+        newestIndex = this.front,
         deletedData;
+    var message = {
+            failure: 'Failure: non-existent node in this list.'
+        };
  
     if (oldestIndex !== newestIndex) {
         deletedData = this._storage[oldestIndex];
         delete this._storage[oldestIndex];
-        this._oldestIndex++;
+        this.rear++;
  
         return deletedData;
+    }
+    else{
+        throw new Error(message.failure);
     }
 };
