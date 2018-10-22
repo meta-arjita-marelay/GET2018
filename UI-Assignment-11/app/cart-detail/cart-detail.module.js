6f8d21cddd26c9980b8b2629bd6a6137c2789a21
Arjita Marelay <arjita.marelay@metacube.com>
2018-10-22 16:53:59 +0530

    UI-Assignment-11

--- UI-Assignment-11/app/cart-detail/cart-detail.module.js
+++
@@ -1,5 +0,0 @@
-'use strict';
-
-angular.module('cartDetail',[
-    'ngRoute'
-]);
--- UI-Assignment-11/app/core/category/category.filter.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-angular.module('core').
-    filter('category',function(){
-        return function(input){
-            
-        }
-    });
